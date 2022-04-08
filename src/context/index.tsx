97ac6cfb7fbea8f3a2b46da30c7ecc7ef903a424
manuelperche <manuelperche@gmail.com>
2022-04-08 17:07:05 -0400

    testing json calls

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { StudentOrder, StudentContextType, StudentInfo } from "../types";
 
 const endpoints = [
-  "http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/",
-  "http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders/",
+  "https://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/",
+  "https://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders/",
 ];
 
 const APIContext = createContext<StudentContextType | null>(null);
@@ -17,17 +17,22 @@ export const APIContextProvider: React.FC<React.ReactNode> = ({ children }) => {
 
   useEffect(() => {
     const fetchData = () => {
-      Promise.all(endpoints.map((endpoint) => axios.get(endpoint, {
-        headers: {
-          'hash': 'OcJn4jYChW'
-        }
-      })))
+      Promise.all(
+        endpoints.map((endpoint) =>
+          axios.get(endpoint, {
+            headers: {
+              hash: "OcJn4jYChW",
+            },
+          })
+        )
+      )
         .then(([{ data: student }, { data: orders }]) => {
           setStudent(student);
           setOrders(orders);
           setLoading(false);
         })
         .catch((error) => {
+          axios.get("orders.json", { headers: { hash: "OcJn4jYChW" } }).then((res => setOrders(res.data)))
           setError(error);
           setLoading(false);
         });
@@ -46,9 +51,9 @@ export const useStudent = () => {
   return {
     student: context.student,
     loading: context.loading,
-    error: context.error
-  }
-}
+    error: context.error,
+  };
+};
 
 export const useOrders = () => {
   const context = useContext(APIContext);
@@ -58,6 +63,6 @@ export const useOrders = () => {
   return {
     orders: context.orders,
     loading: context.loading,
-    error: context.error
-  }
-}
+    error: context.error,
+  };
+};
