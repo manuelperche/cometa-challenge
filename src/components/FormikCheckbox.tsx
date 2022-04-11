1aa918f546d8e9bff4d625f1598ca04e6b1507ba
manuelperche <manuelperche@gmail.com>
2022-04-11 09:42:38 -0400

    code cleanup

--- src/components/FormikCheckbox.tsx
+++ src/components/FormikCheckbox.tsx
@@ -1,17 +1,16 @@
-import { Button, Checkbox } from "@mui/material";
+import { Checkbox } from "@mui/material";
 import { FieldConfig, useField, useFormikContext } from "formik";
 import React from "react";
 
 interface Props extends FieldConfig {
   index: number;
-  disabled?: boolean;
 }
 
 const FormikCheckbox = (props: Props) => {
-  const { index, disabled } = props;
-  const [field, meta] = useField(props);
+  const { index } = props;
+  const [field] = useField(props);
   const { values } = useFormikContext<any>();
-  return <Checkbox {...field} disabled={index > values.checked.length }/>;
+  return <Checkbox {...field} disabled={index > values.checked.length} />;
 };
 
 export default FormikCheckbox;
